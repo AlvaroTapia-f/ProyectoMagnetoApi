22ca3dc81b143efeacbab05a5ddf51b2ee1693a3
Alvaro Tapia <tapia.alvaro.02@gmail.com>
2024-11-02 16:54:51 -0300

    agregando jacoco plugin para codecoverage

--- src/main/java/com/example/magnetoAPI/servicios/BaseService.java
+++ src/main/java/com/example/magnetoAPI/servicios/BaseService.java
@@ -6,13 +6,4 @@ import java.util.List;
 
 public interface BaseService<E extends Base, ID extends Serializable> extends Serializable{
 
-    public List<E> findAll() throws Exception;
-
-    public E findById(ID id) throws Exception;
-
-    public E save(E entidad) throws Exception;
-
-    public E update(ID id, E entidad) throws Exception;
-
-    public boolean delete(ID id) throws Exception;
 }
--- src/main/java/com/example/magnetoAPI/servicios/BaseServiceImpl.java
+++ src/main/java/com/example/magnetoAPI/servicios/BaseServiceImpl.java
@@ -2,10 +2,7 @@ package com.example.magnetoAPI.servicios;
 
 import com.example.magnetoAPI.entidades.Base;
 import com.example.magnetoAPI.repositorios.BaseRepository;
-import jakarta.transaction.Transactional;
 import java.io.Serializable;
-import java.util.List;
-import java.util.Optional;
 
 public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> implements BaseService<E, ID> {
 
@@ -16,64 +13,4 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
         this.baseRepository = baseRepository;
     }
 
-    @Override
-    @Transactional
-    public List<E> findAll() throws Exception {
-        try {
-            List<E> entities = baseRepository.findAll();
-            return entities;
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    @Transactional
-    public E findById(ID id) throws Exception {
-        try {
-            Optional<E> entityOptional = baseRepository.findById(id);
-            return entityOptional.get();
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    @Transactional
-    public E save(E entidad) throws Exception {
-        try {
-            entidad = baseRepository.save(entidad);
-            return entidad;
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    @Transactional
-    public E update(ID id, E entidad) throws Exception {
-        try {
-            Optional<E> entityOptional = baseRepository.findById(id);
-            E entityUpdate = entityOptional.get();
-            entityUpdate = baseRepository.save(entidad);
-            return entityUpdate;
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    @Transactional
-    public boolean delete(ID id) throws Exception {
-        try {
-            if (baseRepository.existsById(id)){
-                baseRepository.deleteById(id);
-                return true;
-            } else {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
-    }
 }
